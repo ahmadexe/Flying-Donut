9dffdd59fe56b7e43f5c5c4f77442ced3a3726e0
ahmadexe <muahmad710@gmail.com>
2023-02-18 23:54:25 +0500

    donut works now

--- donut.c
+++ donut.c
@@ -13,34 +13,33 @@ int main(int argc, char const *argv[])
     {
         memset(b, 32, 1760);
         memset(z, 0, 7040);
-        for (j = 0; 6.28 > j; j += 0.01)
+        for (j = 0; 6.28 > j; j += 0.004)
         {
-            for (i = 0; 6.28 > i; i += 0.08)
+            for (i = 0; 6.28 > i; i += 0.035)
             {
-                float c = sin(i), d = cos(j), e = sin(A), f = sin(j), g = cos(A), 
-                h = d + 2, D = 1 / (c * h * e + f * g + 5), 
-                l = cos(i), m = cos(B), n = sin(B), 
-                t = c * h * g - f * e;
+                float c = sin(i), d = cos(j), e = sin(A), f = sin(j), g = cos(A),
+                      h = d + 2, D = 1 / (c * h * e + f * g + 5),
+                      l = cos(i), m = cos(B), n = sin(B),
+                      t = c * h * g - f * e;
 
                 int x = 40 + 30 * D * (l * h * m - t * n),
-                    y = 12 + 15 * D * (l * h * n + t * m), o = x + 80 * y, 
+                    y = 12 + 15 * D * (l * h * n + t * m), o = x + 80 * y,
                     N = 8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n);
 
-                if (22 > y && y > 0 && 80 > x && D > z[o])
+                if (22 > y && y > 0 && 80 > x && x > 0 && D > z[o])
                 {
                     z[o] = D;
                     b[o] = ".,-~:;=!*#$@:"[N > 0 ? N : 0];
                 }
-
-                printf("\x1b[H");
-                for (k = 0; 1761 > k; k++)
-                {
-                    putchar(k % 80 ? b[k] : 10);
-                }
-                A += 0.04;
-                B += 0.02;
             }
         }
+         printf("\x1b[H");
+            for (k = 0; 1761 > k; k++)
+            {
+                putchar(k % 80 ? b[k] : 10);
+            }
+            A += 0.04;
+            B += 0.02;
     }
 
     return 0;
